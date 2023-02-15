3d5e793757100cb337004641f3ed62d13080c64b
zinstack625 <klimanov625@outlook.com>
2023-02-15 09:36:40 +0300

    check_me: allow testing labs to be sent

--- bot/slash_commands.go
+++ bot/slash_commands.go
@@ -29,7 +29,7 @@ func (b *Bot) checkme(resp http.ResponseWriter, req *http.Request) {
 		resp.Write([]byte("Wrong token secret"))
 	}
 	labUrl := req.Form.Get("text")
-	if ok, err := regexp.Match("^https://github.com/.*/[0-9]{2}-lab-[0-9]{2}.*/pull/[0-9]{1,}$", []byte(labUrl)); err == nil && !ok {
+	if ok, err := regexp.Match("^https://github.com/.*/(?:(?:[0-9]{2}-lab-[0-9]{2}-.*)|(?:lab-test-[0-9]{1}-.*))/pull/[0-9]{1,}", []byte(labUrl)); err == nil && !ok {
 		utils.RespondEphemeral(resp, "Does not seem like a lab we check! Make sure the URL is in form of \"https://github.com/bmstu-cbeer-20**/**-lab-**-YourName/1\"")
 		return
 	}
